9a2752d9296a4505921a52504fca824e557ec7ac
codermoe <codermoe@gmail.com>
2017-02-04 16:22:46 +0800

    动态加载tabar

--- CMoeWeibo/CMoeWeibo/Classes/Main/CMoeMainViewController.swift
+++ CMoeWeibo/CMoeWeibo/Classes/Main/CMoeMainViewController.swift
@@ -13,8 +13,55 @@ class CMoeMainViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+//        addChildViewControllers()
+        
+        let path = Bundle.main.path(forResource: "MainVCSettings", ofType: "json")
+        if let jsonPath = path {
+            let jsonData = NSData(contentsOfFile: jsonPath)
+            
+            do {
+                let dictArr = try JSONSerialization.jsonObject(with: jsonData as! Data, options: JSONSerialization.ReadingOptions.allowFragments)
+                for dict in dictArr as! [[String : String]] {
+                    addChildViewController(childControllerName: dict["vcName"]!, title: dict["title"]!, imageName: dict["imageName"]!)
+                }
+            } catch  {
+                addChildViewControllers()
+            }
+        }
     }
+    
+    private func addChildViewControllers() {
+        addChildViewController(childControllerName: "CMoeHomeTableViewController", title: "首页", imageName: "tabbar_home")
+        addChildViewController(childControllerName: "CMoeMessageTableViewController", title: "消息", imageName: "tabbar_message_center")
+        addChildViewController(childControllerName: "CMoeDescoverTableViewController", title: "广场", imageName: "tabbar_discover")
+        addChildViewController(childControllerName: "CMoeProfileTableViewController", title: "我", imageName: "tabbar_profile")
+    }
+    
+    private func addChildViewController(childControllerName: String, title: String, imageName: String) {
+        let namespace = Bundle.main.infoDictionary!["CFBundleExecutable"] as! String
+        let cls:AnyClass = NSClassFromString(namespace + "." + childControllerName)!
+        print(cls)
+        let vcCls = cls as! UITableViewController.Type
+        let vc = vcCls.init()
+        
+        vc.title = title
+        vc.tabBarItem.image = UIImage(named: imageName)
+        vc.tabBarItem.selectedImage = UIImage(named: imageName + "_highlighted")
+        
+        tabBar.tintColor = UIColor.orange
+        let nav = UINavigationController(rootViewController: vc)
+        addChildViewController(nav)
+    }
+    
+//    private func addChildViewController(childControllerName: String, title: String, imageName: String) {
+//        let cls = NSClassFromString(childControllerName) as! UIViewController
+//        let nav = UINavigationController()
+//        nav.addChildViewController(cls)
+//        cls.title = title
+//        
+////        let nav = UINavigationController(rootViewController: ))
+//        
+//    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -22,14 +69,6 @@ class CMoeMainViewController: UITabBarController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
